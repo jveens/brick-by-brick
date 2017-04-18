b919e78a5f081d139822a617c3ee85afa273d2db
Jenny Veens <jennyveens@gmail.com>
2017-04-17 23:14:15 -0400

    Put everything in an object

--- public/scripts/brick.js
+++ public/scripts/brick.js
@@ -1,62 +1,64 @@
-console.log('hi!');
+const app = {};
 
-const grid = document.querySelector('#grid');
-const sliders = document.querySelectorAll('input[type="range"]');
-const gridSetup = document.querySelector('#grid-setup');
-const body = document.querySelector('body');
+app.grid = document.querySelector('#grid');
+app.sliders = document.querySelectorAll('input[type="range"]');
+app.gridSetup = document.querySelector('#grid-setup');
+app.body = document.querySelector('body');
 
 const trigger = document.querySelector('[data-trigger-more]');
 
-let bricks = document.querySelector('#bricks').value;
-let cols = document.querySelector('#cols').value;
-let gutters = document.querySelector('#gutters').value;
+app.bricks = document.querySelector('#bricks').value;
+app.cols = document.querySelector('#cols').value;
+app.gutters = document.querySelector('#gutters').value;
 
-function getBlocks(number){
-	const parts = [
-		{
-			width: 2,
-			height: 2,
-			className: 'two-two'
-		},
-		{
-			width: 1,
-			height: 1,
-			className: 'one-one'
-		},
-		{
-			width: 2,
-			height: 1,
-			className: 'two-one'
-		},
-		{
-			width: 1,
-			height: 2,
-			className: 'one-two'
-		}
-	];
+app.parts = [
+	{
+		width: 2,
+		height: 2,
+		className: 'two-two'
+	},
+	{
+		width: 1,
+		height: 1,
+		className: 'one-one'
+	},
+	{
+		width: 2,
+		height: 1,
+		className: 'two-one'
+	},
+	{
+		width: 1,
+		height: 2,
+		className: 'one-two'
+	}
+];
+
+app.getBlocks = function(number){
+	
 	const blocks = [];
 
 	for (let i = 0; i <= number; i++) {
-		let rand = Math.floor(Math.random()*parts.length);
+		let rand = Math.floor(Math.random()*app.parts.length);
 
-		blocks.push( parts[rand]);
+		blocks.push( app.parts[rand]);
 	}
 
 	return blocks;
 }
 
-function brickster(number, columns, gutters, clear) {
+app.brickster = function(number, columns, gutters, clear) {
 
-	var blocks = getBlocks(number);
+	var blocks = app.getBlocks(number);
 
 	if (clear) {
 		grid.innerHTML = '';
 	}
 	
-	grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-	grid.style.gridColumnGap = `${gutters}px`;
-	grid.style.gridRowGap = `${gutters}px`;
-	body.style.padding = `${gutters}px`;
+	app.grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+	app.grid.style.gridColumnGap = `${gutters}px`;
+	app.grid.style.gridRowGap = `${gutters}px`;
+	app.body.style.padding = `${gutters}px`;
 
 	blocks.forEach(block => {
 		const gridItem = document.createElement('div');
@@ -66,30 +68,30 @@ function brickster(number, columns, gutters, clear) {
 	});
 }
 
-function updateValues(){
+app.updateValues = function(){
 
-	bricks = document.querySelector('#bricks').value;
-	cols = document.querySelector('#cols').value;
-	gutters = document.querySelector('#gutters').value;
+	app.bricks = document.querySelector('#bricks').value;
+	app.cols = document.querySelector('#cols').value;
+	app.gutters = document.querySelector('#gutters').value;
 
 	const output = document.querySelector(`output[for="${this.id}"]`);
 	output.innerHTML = this.id === 'gutters' ? this.value + 'px' : this.value;
 }
 
-sliders.forEach(slide => {
-	slide.addEventListener('mousemove', updateValues);
-	slide.addEventListener('change', updateValues);
+app.sliders.forEach(slide => {
+	slide.addEventListener('mousemove', app.updateValues);
+	slide.addEventListener('change', app.updateValues);
 });
 
-gridSetup.addEventListener('submit', function(event){
+app.gridSetup.addEventListener('submit', function(event){
 	event.preventDefault();
-	console.log( bricks, cols, gutters );
-	brickster(bricks, cols, gutters, true);
+	console.log( app.bricks, app.cols, app.gutters );
+	app.brickster(app.bricks, app.cols, app.gutters, true);
 });
 
 trigger.addEventListener('click', function(event){
 	event.preventDefault();
-	brickster(bricks, cols, gutters, false);
+	app.brickster(app.bricks, app.cols, app.gutters, false);
 });
 
-brickster(bricks, cols, gutters, false);
+app.brickster(app.bricks, app.cols, app.gutters, false);
